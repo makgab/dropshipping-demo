425cdfc1caae2d50453581137b92ce87c19ba6c6
makgab <makgab@gmail.com>
2026-03-12 14:47:20 +0100

    Update Dockerfile, main.py, index.html for Google Firebase

--- backend/main.py
+++ backend/main.py
@@ -35,5 +35,5 @@ def calc(data: ProfitRequest):
     return {"profit": profit, "margin_percent": margin_percent}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
